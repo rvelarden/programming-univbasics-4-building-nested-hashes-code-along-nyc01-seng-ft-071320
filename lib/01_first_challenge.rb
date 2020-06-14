e7654ff61029b310281b0a2b14e9c43ac121db4d
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-14 21:44:40 +0000

    Automatically backed up by Learn

--- lib/01_first_challenge.rb
+++ lib/01_first_challenge.rb
@@ -1,5 +1,17 @@
 def first_challenge
-   epic_tragedy = {montague: {:patriarch, :matriarch, :hero, :hero_friends}, capulet: {:patriarch, :matriarch, :heroine, :heroine_friends} 
+ epic_tragedy = {
+   :montague => {
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
    }
+}
   
 end
